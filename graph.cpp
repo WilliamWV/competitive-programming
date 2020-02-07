f5e2315393a6fc40bf8ef3f6fe749fbb3ff6b5b4
wwvargas <william.w.vargas@gmail.com>
2020-02-07 18:07:45 -0300

    Add header of functions to manipulate graphs

--- graph.cpp
+++ graph.cpp
@@ -10,11 +10,15 @@ typedef struct{
     int nodes;
 } Graph;
 
-void dfs(Graph graph, int src);
-void bfs(Graph graph, int src);
-int dijkstra(Graph graph, int src, int dst);
-int* complete_dijkstra(Graph graph, int src);
-Graph dijkstra_graph(Graph graph, int src);
-int kruskal_cost(Graph graph);
-Graph mst(Graph graph);
-int** floyd_warshall(Graph graph);
+Graph* init_graph(int nodes);
+void add_edge(Graph* graph, int src, int dst, int weight);
+void rm_edge(Graph* graph, int src, int dst);
+void print_graph(Graph* graph);
+void dfs(Graph* graph, int src);
+void bfs(Graph* graph, int src);
+int dijkstra(Graph* graph, int src, int dst);
+int* complete_dijkstra(Graph* graph, int src);
+Graph* dijkstra_graph(Graph* graph, int src);
+int kruskal_cost(Graph* graph);
+Graph* mst(Graph* graph);
+int** floyd_warshall(Graph* graph);
