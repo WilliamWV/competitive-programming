71e450b7bef733cb78c51d3f2471f4bb10eebad4
wwvargas <william.w.vargas@gmail.com>
2020-02-14 12:14:36 -0300

    Start using googletest to test searching functions

--- test/searching_test.cpp
+++ test/searching_test.cpp
@@ -1,30 +1,59 @@
 #include "../include/searching.h"
 #include "test.h"
+#include <gtest/gtest.h>
 
 using namespace std;
 
-int test_searching(){
-    
-    vector<int> o1 = {1, 4, 8, 9, 10, 11, 12, 18, 21};
-    vector<int> o2 = {0, 9, 192, 92830, 12723937, 128384621};
-    vector<int> o3 = {1, 2, 8, 9, 17, 93};
-    cout<< "BINARY SEARCH" <<endl;
-    cout<< "Index of element 8 on o1: " << binary_search(o1, 8)<<endl;
-    cout<< "Index of element 12 on o1: " << binary_search(o1, 12)<<endl;
-    cout<< "Index of element 0 on o2: " << binary_search(o2, 0)<<endl;
-    cout<< "Index of element 93 on o3: " << binary_search(o3, 93)<<endl;
-    cout<< "Index of element -42 on o1: " << binary_search(o1,-42)<<endl;
+vector<int> o1 = {1, 4, 8, 9, 10, 11, 12, 18, 21};
+vector<int> o2 = {0, 9, 192, 92830, 12723937, 128384621};
+vector<int> o3 = {1, 2, 8, 9, 17, 93};
 
-    vector<int> u1 = {1, 4, 8, 9, 10, 11, 12, 18, 21};
-    vector<int> u2 = {1, 5, 8, 2, 3, -9, 19};
-    vector<int> u3 = {42, 9, 1, 56, 2, 3, 78, 1, 0};
+
+TEST(BinarySearchTest, ElementNotPresent){
+    ASSERT_EQ(-1, binary_search(o1, 7));
+    ASSERT_EQ(-1, binary_search(o2, 100));
+    ASSERT_EQ(-1, binary_search(o3, 94));
+    ASSERT_EQ(-1, binary_search(o1, -2));
+    ASSERT_EQ(-1, binary_search(o2, 1));
+}
+
+TEST(BinarySearchTest, ElementPresent){
+    ASSERT_EQ(0, binary_search(o1, 1));
+    ASSERT_EQ(8, binary_search(o1, 21));
+    ASSERT_EQ(2, binary_search(o2, 192));
+    ASSERT_EQ(4, binary_search(o3, 17));
+    ASSERT_EQ(1, binary_search(o2, 9));
     
-    cout<< "LINEAR SEARCH" <<endl;
-    cout<< "Index of element 8 on u1: " << linear_search(u1, 8)<<endl;
-    cout<< "Index of element 2 on u2: " << linear_search(u2, 2)<<endl;
-    cout<< "Index of element 0 on u2: " << linear_search(u2, 0)<<endl;
-    cout<< "Index of element 78 on u3: " << linear_search(u3, 78)<<endl;
-    cout<< "Index of element 42 on u3: " << linear_search(u3,42)<<endl;
+}
+
+vector<int> u1 = {1, 4, 8, 9, 10, 11, 12, 18, 21};
+vector<int> u2 = {1, 5, 8, 2, 3, -9, 19};
+vector<int> u3 = {42, 9, 1, 56, 2, 3, 78, 1, 0};
     
+
+TEST(LinearSearchTest,ElementNotPresent){
+    ASSERT_EQ(-1, linear_search(u1, 0));
+    ASSERT_EQ(-1, linear_search(u2, 25));
+    ASSERT_EQ(-1, linear_search(u3, 13));
+    ASSERT_EQ(-1, linear_search(u1, -3));
+    ASSERT_EQ(-1, linear_search(u2, 12));
+    
+}
+
+TEST(LinearSearchTest, ElementPresent){
+    ASSERT_EQ(0, linear_search(u1, 1));
+    ASSERT_EQ(2, linear_search(u1, 8));
+    ASSERT_EQ(6, linear_search(u2, 19));
+    ASSERT_EQ(3, linear_search(u3, 56));
+    ASSERT_EQ(5, linear_search(u2, -9));
+    
+}
+
+int test_searching(){
     return OK;
+}
+
+int main(int argc, char **argv){
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
