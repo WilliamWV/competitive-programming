dd41ab175b3057e78698d31a561de1f6b43502ea
wwvargas <william.w.vargas@gmail.com>
2020-02-16 19:02:11 -0300

    Testing sorting functions with googletest

--- test/sorting_test.cpp
+++ test/sorting_test.cpp
@@ -1,35 +1,87 @@
 #include "../include/sorting.h"
+#include <gtest/gtest.h>
 
 using namespace std;
 
-int test_sorting(){
+vector<int> u1 = {4, 6, 2, 1, 6, 7, 2, 9, 10, -1, 0, 4, 6};
+vector<int> u2 = {4};
+vector<int> u3 = {4, 6, 2, 1, 6, 7, 2, 9, 10, -1, 0, 4, 6, 0, 0, 2, 4, 876, -13, 87, 23, 0, 14, -12};
+vector<int> u4 = {4, 2, 1, 6, 7, 9, 10, -1, 0};
+vector<int> u5 = {-1, -1, -1, 0, -1, -1, 2, -4};
+
+vector<int> o1 = {-1, 0, 1, 2, 2, 4, 4, 6, 6, 6, 7, 9, 10};
+vector<int> o2 = {4};
+vector<int> o3 = {-13, -12, -1, 0, 0, 0, 0, 1, 2, 2, 2, 4, 4, 4, 6, 6, 6, 7, 9, 10, 14, 23, 87, 876};
+vector<int> o4 = {-1, 0, 1, 2, 4, 6, 7, 9, 10};
+vector<int> o5 = {-4, -1, -1, -1, -1, -1, 0, 2};
+
+template <typename T>
+bool vectorCompare(vector<T> &v1, vector<T> &v2){
+    if (v1.size() != v2.size()){
+        return false;
+    } 
+    for (int i = 0; i < v1.size(); i++){
+        if (v1[i] != v2[i]){ 
+            return false;
+        }
+    }
+    return true;
+}
+
+void reinitVectors(){
+    u1 = {4, 6, 2, 1, 6, 7, 2, 9, 10, -1, 0, 4, 6};
+    u2 = {4};
+    u3 = {4, 6, 2, 1, 6, 7, 2, 9, 10, -1, 0, 4, 6, 0, 0, 2, 4, 876, -13, 87, 23, 0, 14, -12};
+    u4 = {4, 2, 1, 6, 7, 9, 10, -1, 0};
+    u5 = {-1, -1, -1, 0, -1, -1, 2, -4};
+}
+
+
+TEST(QuicksortTest, QSTest){
+    reinitVectors();
+
+    quicksort(u1);
+    quicksort(u2);
+    quicksort(u3);
+    quicksort(u4);
+    quicksort(u5);
     
-    vector<int> v1 = {4, 6, 2, 1, 6, 7, 2, 9, 10, -1, 0, 4, 6};
-    vector<int> v2 = {4};
-    vector<int> v3 = {4, 6, 2, 1, 6, 7, 2, 9, 10, -1, 0, 4, 6, 0, 0, 2, 4, 876, -13, 87, 23, 0, 14, -12};
-    vector<int> v4 = {4, 2, 1, 6, 7, 9, 10, -1, 0};
-    vector<int> v5 = {-1, -1, -1, 0, -1, -1, 2, -4};
+    ASSERT_TRUE(vectorCompare(o1, u1));
+    ASSERT_TRUE(vectorCompare(o2, u2));
+    ASSERT_TRUE(vectorCompare(o3, u3));
+    ASSERT_TRUE(vectorCompare(o4, u4));
+    ASSERT_TRUE(vectorCompare(o5, u5));
+
+}
+
+TEST(MergesortTest, MSTest){
+    reinitVectors();
+
+    mergesort(u1);
+    mergesort(u2);
+    mergesort(u3);
+    mergesort(u4);
+    mergesort(u5);
     
+    ASSERT_TRUE(vectorCompare(o1, u1));
+    ASSERT_TRUE(vectorCompare(o2, u2));
+    ASSERT_TRUE(vectorCompare(o3, u3));
+    ASSERT_TRUE(vectorCompare(o4, u4));
+    ASSERT_TRUE(vectorCompare(o5, u5));
+}
 
+TEST(BublesortTest, BSTest){
+    reinitVectors();
     
-    print_vec(v1, "Initial vector");
-    sort(v1);
-    print_vec(v1, "Final vector");
-
-    print_vec(v2, "Initial vector");
-    sort(v2);
-    print_vec(v2, "Final vector");
-
-    print_vec(v3, "Initial vector");
-    sort(v3);
-    print_vec(v3, "Final vector");
-
-    print_vec(v4, "Initial vector");
-    sort(v4);
-    print_vec(v4, "Final vector");
-
-    print_vec(v5, "Initial vector");
-    sort(v5);
-    print_vec(v5, "Final vector");
-
+    bubblesort(u1);
+    bubblesort(u2);
+    bubblesort(u3);
+    bubblesort(u4);
+    bubblesort(u5);
+    
+    ASSERT_TRUE(vectorCompare(o1, u1));
+    ASSERT_TRUE(vectorCompare(o2, u2));
+    ASSERT_TRUE(vectorCompare(o3, u3));
+    ASSERT_TRUE(vectorCompare(o4, u4));
+    ASSERT_TRUE(vectorCompare(o5, u5));
 }
